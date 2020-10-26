dbd40973cd2a59f7f767c8f3edd93f5b521b8df8
trunganh98 <trunganh98@users.noreply.github.com>
2020-10-26 18:24:12 +0700

    lab10

---
+++ UWP_Weather/UWP_Weather/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP_Weather
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RootObject myWeather = await OpenWeatherMapProxy.GetWether(21.027763, 105.834160);
+            string icon = String.Format("http://Openweathermap.org/img/w/{0}.png", myWeather.weather[0].icon);
+            ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+            //ResultTextBlock.Text = myWeather.name ;
+            ResultTextBlock.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+
+            var position = await LocationManager.GetPosition();
+        }
+    }
+}
---
+++ UWP_Weather/UWP_Weather/OpenWeatherMapProxy.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UWP_Weather
+{
+    public class OpenWeatherMapProxy
+    {
+
+        public async static Task<RootObject> GetWether(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("http://api.openweathermap.org/data/2.5/weather?lat=21.027763&lon=105.834160&units=imperial&appid=e884a9d1405ed3d3291b00819931ff32"); 
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+
+        [DataMember]
+        public double lat { get; set; }
+    }
+
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string main { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+    }
+
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+        [DataMember]
+        public double feels_like { get; set; }
+        [DataMember]
+        public double temp_min { get; set; }
+        [DataMember]
+        public double temp_max { get; set; }
+        [DataMember]
+        public double pressure { get; set; }
+        [DataMember]
+        public double humidity { get; set; }
+    }
+
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+        [DataMember]
+        public double deg { get; set; }
+    }
+
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+
+    [DataContract]
+    public class Sys
+    {
+        [DataMember]
+        public int type { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public int sunrise { get; set; }
+        [DataMember]
+        public int sunset { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public Coord coord { get; set; }
+        [DataMember]
+        public List<Weather> weather { get; set; }
+        [DataMember]
+        public string @base { get; set; }
+        [DataMember]
+        public Main main { get; set; }
+        [DataMember]
+        public int visibility { get; set; }
+        [DataMember]
+        public Wind wind { get; set; }
+        [DataMember]
+        public Clouds clouds { get; set; }
+        [DataMember]
+        public int dt { get; set; }
+        [DataMember]
+        public Sys sys { get; set; }
+        [DataMember]
+        public int timezone { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public int cod { get; set; }
+    }
+
+}
